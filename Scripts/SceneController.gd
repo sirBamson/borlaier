2355e4fdf11d040e37cee9c0b6cf6a28ec6ca2ac
sirVader <alvar.m.sjogren@gmail.com>
2022-11-13 22:10:43 +0100

    Changed to ElevatorControll

--- Scripts/SceneController.gd
+++ Scripts/SceneController.gd
@@ -1,19 +1,21 @@
 extends Node
 
-var last_scene 
 
-
-func change_to_scene(current_scene: Node, save_current_scene: bool, new_scene_path: String) -> void:
-	if save_current_scene:
-		var new_scene = last_scene.instance()
-		add_child(new_scene)
-	else:
-		var new_scene = load(new_scene_path).instance()
-		add_child(new_scene)
-		last_scene = PackedScene.new()
-		last_scene.pack(current_scene)
-		
+func change_scene(save_current_scene: bool, current_scene: Node, current_scene_path: String, new_scene_path: String):
+	# Add a tansition
+	var new_scene: Node
 	
-	# Add a transition
-	# Call last
+	if save_current_scene:
+			var saved_scene = PackedScene.new()
+			saved_scene.pack(current_scene)
+			EnvVar.saved_scenes[current_scene_path] = saved_scene
+	
+	if new_scene_path in EnvVar.saved_scenes:
+		new_scene = EnvVar.saved_scenes[new_scene_path].instance()
+	else:
+		new_scene = load(new_scene_path).instance()
+
+	add_child(new_scene)
+	
 	current_scene.queue_free()
+	
