b734d4393d9f99599fdb48122e565c1223c6c927
sirVader <alvar.m.sjogren@gmail.com>
2022-12-09 11:02:40 +0100

    Added toggle auto mode to wepons

--- Scripts/Gun.gd
+++ Scripts/Gun.gd
@@ -1,20 +1,31 @@
 extends Sprite
 
-export (int) var bullet_speed = 1000
+
+export (int) var bullet_speed = 1500
 export (int) var magasine_size = 20
 export (float) var fire_rate = 0.12
 export (int) var damage = 10
 export (bool) var auto = false
 
 
+
+var bullets_in_mag: int
+
+
 var bullet = load("res://Scenes/Bullet.tscn")
 var can_fire: bool = true
-var fire_pressed: bool = false
+var can_fire_auto: bool = true
+
 
 func _ready() -> void:
-	PlayerGlobals.magazine_size = magasine_size
-	PlayerGlobals.bullets_in_mag = magasine_size
+	reload_gun()
 
+
+func _input(event: InputEvent) -> void:
+
+	if event.is_action_released("fire"):
+		can_fire_auto = true
+		print("can fire")
 
 
 func _physics_process(delta: float) -> void:
@@ -26,23 +37,24 @@ func _physics_process(delta: float) -> void:
 		scale = Vector2(1, 1)
 	
 	
-	if Input.is_action_just_pressed("reload") and PlayerGlobals.bullets_in_mag < 25 and PlayerGlobals.bullets_left > 0 and can_fire:
-		#PlayerGlobals.bullets_left += PlayerGlobals.bullets_in_mag
+	if auto:
+		can_fire_auto = true
+	
+	
+	if Input.is_action_just_pressed("reload") and bullets_in_mag < magasine_size and PlayerGlobals.bullets_left > 0 and can_fire:
 		
 		$GunReloadSound.play()
-		for i in range(magasine_size): 
-			if PlayerGlobals.bullets_left != 0 and !PlayerGlobals.bullets_in_mag >= 25:
-				PlayerGlobals.bullets_left -= 1
-				PlayerGlobals.bullets_in_mag += 1
+		reload_gun()
 		
 		can_fire = false
 		yield(get_tree().create_timer(1.5),"timeout")
 		can_fire = true
 	
 	
-	if Input.is_action_pressed("fire") and can_fire and PlayerGlobals.bullets_in_mag > 0:
+	if Input.is_action_pressed("fire") and can_fire and can_fire_auto and bullets_in_mag > 0:
+		can_fire_auto = false
 		
-		PlayerGlobals.bullets_in_mag -= 1
+		bullets_in_mag -= 1
 		$BulletSound.play()
 		var bullet_instance = bullet.instance()
 		bullet_instance.global_position = $BulletSpawn.global_position
@@ -55,7 +67,8 @@ func _physics_process(delta: float) -> void:
 		yield(get_tree().create_timer(fire_rate),"timeout")
 		can_fire = true
 	
-	elif Input.is_action_pressed("fire") and can_fire and PlayerGlobals.bullets_in_mag == 0:
+	elif Input.is_action_pressed("fire") and can_fire and can_fire_auto and bullets_in_mag == 0:
+		can_fire_auto = false
 		
 		$GunClickSound.play()
 		
@@ -63,3 +76,13 @@ func _physics_process(delta: float) -> void:
 		can_fire = false
 		yield(get_tree().create_timer(fire_rate),"timeout")
 		can_fire = true
+	
+	PlayerGlobals.bullets_in_mag = bullets_in_mag
+
+
+
+func reload_gun() -> void:
+	for i in range(magasine_size): 
+		if PlayerGlobals.bullets_left != 0 and !bullets_in_mag >= magasine_size:
+			PlayerGlobals.bullets_left -= 1
+			bullets_in_mag += 1
