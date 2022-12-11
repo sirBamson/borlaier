9c9b25d542e9d2cb1fbaf185a92aba4294905948
sirVader <alvar.m.sjogren@gmail.com>
2022-12-11 17:47:41 +0100

    Changes to transition

--- Scripts/Elevator/ElevatorPanel.gd
+++ Scripts/Elevator/ElevatorPanel.gd
@@ -3,6 +3,12 @@ extends Area2D
 
 # If player can use the panel
 var can_use_panel: bool = false
+var animation_done: bool = false
+
+
+func _ready() -> void:
+	# Connects signal to the Transition node
+	var _err = Transition.get_node("Animation").connect("animation_finished", self, "transition_done")
 
 
 # Checks if the player is in the designated area
@@ -15,8 +21,13 @@ func _on_ElevatorPanel_area_exited(area: Area2D) -> void:
 		can_use_panel = false
 
 
+# Checks that the transition is complete
+func transition_done(_anim_name: String) -> void:
+	animation_done = true
+
+
 func _physics_process(_delta: float) -> void:
-	if can_use_panel:
+	if can_use_panel and animation_done:
 		if Input.is_action_pressed("ui_accept"):
 			get_parent().get_node("ElevatorPanelUI").visible = true
 			get_tree().paused = true
