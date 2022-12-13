db5c984c4176e7289a84a3f63f4aed40e776644d
sirVader <alvar.m.sjogren@gmail.com>
2022-12-13 12:16:29 +0100

    Moved transition node

--- Scripts/Elevator/ElevatorPanel.gd
+++ Scripts/Elevator/ElevatorPanel.gd
@@ -6,10 +6,6 @@ var can_use_panel: bool = false
 var animation_done: bool = false
 
 
-func _ready() -> void:
-	# Connects signal to the Transition node
-	var _err = Transition.get_node("Animation").connect("animation_finished", self, "transition_done")
-
 
 # Checks if the player is in the designated area
 func _on_ElevatorPanel_area_entered(area: Area2D) -> void:
@@ -21,13 +17,8 @@ func _on_ElevatorPanel_area_exited(area: Area2D) -> void:
 		can_use_panel = false
 
 
-# Checks that the transition is complete
-func transition_done(_anim_name: String) -> void:
-	animation_done = true
-
-
 func _physics_process(_delta: float) -> void:
-	if can_use_panel and animation_done:
+	if can_use_panel:
 		if Input.is_action_pressed("ui_accept"):
 			get_parent().get_node("ElevatorPanelUI").visible = true
 			get_tree().paused = true
