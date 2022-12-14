2a9ca9a316d1f2a919b4833157ee83f64f63264d
sirVader <alvar.m.sjogren@gmail.com>
2022-12-14 08:50:50 +0100

    Small player fixes

--- Scripts/AutoLoads/PlayerGlobals.gd
+++ Scripts/AutoLoads/PlayerGlobals.gd
@@ -6,3 +6,5 @@ var player_has_gun: bool = true
 var magazine_size: int = 20
 var bullets_in_mag: int = 25
 var bullets_left: int = 200
+
+var current_weapon: String = "res://Scenes/Weapons/AssultRifle.tscn"
