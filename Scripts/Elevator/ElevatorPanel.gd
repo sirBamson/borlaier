d56c7ff808d1b9004ee3312cd0a6db7e1341d684
sirVader <alvar.m.sjogren@gmail.com>
2023-01-09 22:33:18 +0100

    New pause function

--- Scripts/Elevator/ElevatorPanel.gd
+++ Scripts/Elevator/ElevatorPanel.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 # If player can use the panel
 var can_use_panel: bool = false
+var panel_current: bool = false
 var animation_done: bool = false
 
 
@@ -19,9 +20,16 @@ func _on_ElevatorPanel_area_exited(area: Area2D) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if can_use_panel:
-		if Input.is_action_pressed("ui_accept"):
+		if Input.is_action_pressed("ui_accept") and not panel_current and not get_tree().paused:
+			panel_current = true
 			get_parent().get_node("ElevatorPanelUI").visible = true
 			get_tree().paused = true
-		if Input.is_action_pressed("ui_cancel"):
+			
+			EnvVar.can_pause = false
+		
+		if Input.is_action_pressed("ui_cancel") and panel_current:
+			panel_current = false
 			get_parent().get_node("ElevatorPanelUI").visible = false
 			get_tree().paused = false
+			
+			EnvVar.can_pause = true
