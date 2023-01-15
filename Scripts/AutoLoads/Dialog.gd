a1be8e0db684efc803a97671cf4f1b91002484fb
sirVader <alvar.m.sjogren@gmail.com>
2023-01-15 13:19:06 +0100

    Started dialog function

---
+++ Scripts/AutoLoads/Dialog.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+
+var controller: Dictionary = {
+	"test1": 
+		[1,
+		"Hello player",
+		"I have a quest for you"
+		],
+	
+	"test2": 
+		[1,
+		"Hello player",
+		"I have a quest for you"
+		]
+}
