b10612dcc14aadc726e28743443d7e4c04965624
sirVader <alvar.m.sjogren@gmail.com>
2023-01-26 17:32:38 +0100

    Pathfinding fix

--- Scripts/Entities/EnemyBasic.gd
+++ Scripts/Entities/EnemyBasic.gd
@@ -2,19 +2,29 @@ extends KinematicBody2D
 
 
 onready var pathfinding = get_parent().get_parent().get_node("Pathfinding")
-onready var player = get_parent().get_node("Player")
+onready var player: KinematicBody2D = get_parent().get_node("Player")
 
 export (int) var speed: int = 250
 
 var path: Array
+var last_path: Array
 
 
-func _physics_process(delta: float) -> void:
-	movment()
+func _physics_process(_delta: float) -> void:
+	detect_player()
+	move_towards()
 
 
-func movment() -> void:
+func detect_player() -> void:
+	$RayCast.cast_to = player.position
+	print($RayCast)
+
+
+func move_towards() -> void:
+	var velocity: Vector2 = Vector2.ZERO
 	path = pathfinding.get_new_path(global_position, player.global_position)
-	print(path)
 	if path.size() > 2:
-		global_position = global_position.move_toward(path[1], get_process_delta_time() * speed)
+		velocity = global_position.direction_to(path[1])
+		
+		velocity = velocity.normalized() * speed
+		velocity = move_and_slide(velocity)
