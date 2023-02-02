480a77c980b1b4cd07119ef964279e1cb149b2e7
sirVader <alvar.m.sjogren@gmail.com>
2023-02-02 22:09:53 +0100

    Added new buttons

--- Scripts/UI/Pause.gd
+++ Scripts/UI/Pause.gd
@@ -11,3 +11,19 @@ func _physics_process(_delta: float) -> void:
 		if Input.is_action_just_pressed("esc"):
 			visible = !visible
 			get_tree().paused = !get_tree().paused
+	
+	for button in $VBoxContainer/VBoxContainer.get_children():
+		$VBoxContainer/VBoxContainer/Settings.focus_mode
+		#if button.
+
+
+func _on_SaveGame_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_QuitToMenu_pressed() -> void:
+	get_tree().quit()
