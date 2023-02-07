1a2b0043592c8a58b6c8811fce129613ad7d3f6f
sirVader <alvar.m.sjogren@gmail.com>
2023-02-07 22:40:16 +0100

    Added fps counter

--- Scripts/UI/Overlay.gd
+++ Scripts/UI/Overlay.gd
@@ -7,6 +7,7 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	if PlayerGlobals.player_has_gun:
 		$TextureRect/Bullets.visible = true
+		$TextureRect/Bullets/FPS.text = "FPS: " + str(Engine.get_frames_per_second())
 		$TextureRect/Bullets/BulletsInMag.text = "Bullets in mag: " + str(PlayerGlobals.bullets_in_mag)
 		$TextureRect/Bullets/BulletsLeft.text = "Bullets left: " + str(PlayerGlobals.bullets_left)
 	else:
