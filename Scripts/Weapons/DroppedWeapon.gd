6dde58105ac24c6891244c1295d2439d771d57ab
agrumpyoldlady <hampussahlsten22@gmail.com>
2023-02-21 09:41:38 +0100

    Added functioning smg

--- Scripts/Weapons/DroppedWeapon.gd
+++ Scripts/Weapons/DroppedWeapon.gd
@@ -4,7 +4,8 @@ extends Area2D
 onready var weapons: Dictionary = {
 	"res://Scenes/Weapons/AssultRifle.tscn": get_node("AssultRifleShape"),
 	"res://Scenes/Weapons/Pistol.tscn": get_node("PistolShape"),
-	"res://Scenes/Weapons/SemiAutomaticRifle.tscn": get_node("SemiAutomaticRifleShape")
+	"res://Scenes/Weapons/SemiAutomaticRifle.tscn": get_node("SemiAutomaticRifleShape"),
+	"res://Scenes/Weapons/SMG.tscn": get_node("SMGShape")
 	}
 
 
