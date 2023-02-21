8e2199cffb749dcfe48cd9d2b04e92287296fd86
agrumpyoldlady <hampussahlsten22@gmail.com>
2023-02-21 09:43:01 +0100

    CHanged overlay

--- Scripts/UI/Overlay.gd
+++ Scripts/UI/Overlay.gd
@@ -1,19 +1,17 @@
 extends CanvasLayer
 
 func _ready() -> void:
-	$VBox/BulletsInMag.visible = false
-	$VBox/BulletsLeft.visible = false
+	$VBox/Bullets.visible = false
 
 
 func _physics_process(_delta: float) -> void:
 	if PlayerGlobals.has_gun:
-		$VBox/BulletsInMag.visible = true
-		$VBox/BulletsLeft.visible = true
-		$VBox/BulletsInMag.text = "Bullets in mag: " + str(PlayerGlobals.bullets_in_mag)
-		$VBox/BulletsLeft.text = "Bullets left: " + str(PlayerGlobals.bullets_left)
+		$VBox/Bullets.visible = true
+		$VBox/Bullets.text = "     : " + str(PlayerGlobals.bullets_in_mag) + " | " + str(PlayerGlobals.bullets_left)
+
 	else:
-		$VBox/BulletsInMag.visible = false
-		$VBox/BulletsLeft.visible = false
+		$VBox/Bullets.visible = false
 	
 	$VBox/Coins.text = "Money: " + str(PlayerGlobals.coins) + "c"
 	$VBox/FPS.text = "FPS: " + str(Engine.get_frames_per_second())
+	
