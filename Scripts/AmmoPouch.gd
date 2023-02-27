602cf5859461cd0654dd3bf18cbb457b3346dc10
sirVader <alvar.m.sjogren@gmail.com>
2023-02-27 20:20:21 +0100

    Fixed ammo pouch

--- Scripts/AmmoPouch.gd
+++ Scripts/AmmoPouch.gd
@@ -5,20 +5,19 @@ var bullets_in_pouch: int = 50
 func _ready() -> void:
 	$Label.text = "50"
 
+
 func _on_AmmoPouch_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Player"):
-		if PlayerGlobals.bullets_left <= 250:
-			PlayerGlobals.bullets_left += 50
+	if area.get_parent().is_in_group("Player"):
+		# 300 bullets max
+		while bullets_in_pouch > 0:
+			if PlayerGlobals.bullets_left < 300:
+				PlayerGlobals.bullets_left += 1
+				bullets_in_pouch -= 1
+			elif PlayerGlobals.bullets_left >= 300:
+				break
+		
+		if bullets_in_pouch == 0:
 			queue_free()
-		else:
-			bullets_in_pouch -= (300 - PlayerGlobals.bullets_left)
-			PlayerGlobals.bullets_left += (300 - PlayerGlobals.bullets_left)
-			$Label.text = str(bullets_in_pouch)
-			if (300 - PlayerGlobals.bullets_left) > bullets_in_pouch:
-					PlayerGlobals.bullets_left + bullets_in_pouch
-					print(bullets_in_pouch)
-					if bullets_in_pouch <= 0:
-						queue_free()
-					
+		$Label.text = str(bullets_in_pouch)
 
 
