b3ef9e7c6448f67fd477fc5b7bbbdfd3c7c31be2
sirVader <alvar.m.sjogren@gmail.com>
2023-02-27 16:46:14 +0100

    Small enemy spawner changes

--- Scripts/EnemySpawner.gd
+++ Scripts/EnemySpawner.gd
@@ -1,23 +1,25 @@
 extends Position2D
 
 export (String) var enemy_path: String
+export (String) var enemy_name: String
 export (int) var spawn_interval: int
 export (int) var max_enemy_amount: int
+export (bool) var active: bool
 
 onready var enemy: = load(enemy_path)
 
 var enemy_amount: int
-
 
 func _ready() -> void:
 	$SpawnTimer.start(spawn_interval)
 
 
 func _on_SpawnTimer_timeout() -> void:
+	enemy_amount = 0
 	for node in get_parent().get_children():
-		if node.is_in_group("Chipling"):
+		if node.is_in_group(enemy_name):
 			enemy_amount += 1
-	if !(enemy_amount > max_enemy_amount):
+	if enemy_amount < max_enemy_amount and active:
 		var enemy_instance = enemy.instance()
 		enemy_instance.global_position = global_position
 		get_parent().add_child(enemy_instance)
