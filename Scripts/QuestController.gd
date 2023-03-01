8cb79b9128885cc1ffa573fb691154fe9bac2373
sirVader <alvar.m.sjogren@gmail.com>
2023-03-02 00:19:22 +0100

    Added new quests

--- Scripts/QuestController.gd
+++ Scripts/QuestController.gd
@@ -27,6 +27,7 @@ class Quest:
 
 var quests: Array = []
 
+# Fatman quests
 var FatMan1: Quest = Quest.new()
 var FatMan2: Quest = Quest.new()
 
@@ -34,9 +35,22 @@ var FatMan_dialogic: Dictionary = {}
 var FatMan1_dict: Dictionary = {}
 var FatMan2_dict: Dictionary = {}
 
+# Bartender2 quests
+var Bartender21: Quest = Quest.new()
+
+var Bartender2_dialogic: Dictionary = {}
+var Bartender21_dict: Dictionary = {}
+
+
+var Mohamed1: Quest = Quest.new()
+
+var Mohamed_dialogic: Dictionary = {}
+var Mohamed1_dict: Dictionary = {}
+
+
 func _ready() -> void:
 	FatMan1.id_name = "FatMan1"
-	FatMan1.description = "Kill 10 chiplings"
+	FatMan1.description = "Kill 20 chiplings"
 	FatMan1.dependencies = {
 		"FatMan1": false
 	}
@@ -46,8 +60,9 @@ func _ready() -> void:
 	FatMan1.quest_series_number = 1
 	quests.append(FatMan1)
 	
+	
 	FatMan2.id_name = "FatMan2"
-	FatMan2.description = "Kill 10 chiplings"
+	FatMan2.description = "Kill 20 chiplings"
 	FatMan2.dependencies = {
 		"FatMan1": true,
 		"FatMan2": false
@@ -57,6 +72,30 @@ func _ready() -> void:
 	FatMan2.dialogic_next_quest_avalible_name = "Npc/FatMan/FatManNextQuestAvalible"
 	FatMan2.quest_series_number = 2
 	quests.append(FatMan2)
+	
+	
+	Bartender21.id_name = "Bartender21"
+	Bartender21.description = "Get a drink"
+	Bartender21.dependencies = {
+		"FatMan1": true
+	}
+	Bartender21.dialogic_quest_series_name = "Npc/Bartender2/Bartender2CurrentQuest"
+	Bartender21.dialogic_quest_started_name = "Npc/Bartender2/Bartender2QuestStarted"
+	Bartender21.dialogic_next_quest_avalible_name = "Npc/Bartender2/Bartender2NextQuestAvalible"
+	Bartender21.quest_series_number = 1
+	quests.append(Bartender21)
+	
+	
+	Mohamed1.id_name = "Mohamed1"
+	Mohamed1.description = "Get a drink"
+	Mohamed1.dependencies = {
+		"Bartender21": true
+	}
+	Mohamed1.dialogic_quest_series_name = "Npc/Mohamed/MohamedCurrentQuest"
+	Mohamed1.dialogic_quest_started_name = "Npc/Mohamed/MohamedQuestStarted"
+	Mohamed1.dialogic_next_quest_avalible_name = "Npc/Mohamed/MohamedNextQuestAvalible"
+	Mohamed1.quest_series_number = 1
+	quests.append(Mohamed1)
 
 
 
@@ -91,14 +130,14 @@ func check_and_set_quest_objectives(string: String) -> void:
 	# Quest done check
 	elif quest.id_name == "FatMan1" and quest.active:
 		print("Quest check")
-		if Stats.chiplings_killed - chiplings_killed_start >= 2:
+		if Stats.chiplings_killed - chiplings_killed_start >= 20:
 			Dialogic.set_variable("Npc/FatMan/FatManCurrentQuestDone", "true")
 			Dialogic.set_variable("Npc/FatMan/FatManQuestStarted", "false")
 			Dialogic.set_variable("Npc/FatMan/FatManCurrentQuest", "2")
 			quest.completed = true
 			PlayerGlobals.coins += 100
 	
-	# FatMan1 quest objective
+	# FatMan2 quest objective
 	# Quest init
 	if quest.id_name == "FatMan2" and !quest.active:
 		print("Quest start")
@@ -108,11 +147,51 @@ func check_and_set_quest_objectives(string: String) -> void:
 	# Quest done check
 	elif quest.id_name == "FatMan2" and quest.active:
 		print("Quest check")
-		if Stats.chiplings_killed - chiplings_killed_start >= 2:
+		if Stats.chiplings_killed - chiplings_killed_start >= 40:
 			Dialogic.set_variable("Npc/FatMan/FatManCurrentQuestDone", "true")
 			Dialogic.set_variable("Npc/FatMan/FatManQuestStarted", "false")
 			quest.completed = true
-			PlayerGlobals.coins += 100
+			PlayerGlobals.coins += 200
+	
+	# Bartender21 quest objective
+	# Quest init
+	if quest.id_name == "Bartender21" and !quest.active:
+		print("Quest start")
+		Dialogic.set_variable("Npc/Bartender2/Bartender2CurrentQuestDone", "false")
+		chiplings_killed_start = Stats.chiplings_killed
+	
+	# Quest done check
+	elif quest.id_name == "Bartender21" and quest.active:
+		print("Quest check")
+		if Stats.chiplings_killed - chiplings_killed_start >= 2:
+			Dialogic.set_variable("Npc/Bartender2/Bartender2CurrentQuestDone", "true")
+			Dialogic.set_variable("Npc/Bartender2/Bartender2QuestStarted", "false")
+			Dialogic.set_variable("Npc/Bartender2/Bartender2CurrentQuest", "1")
+			quest.completed = true
+			PlayerGlobals.coins += 250
+	
+	# Mohamed1 quest objective
+	# Quest init
+	if quest.id_name == "Mohamed1" and !quest.active:
+		print("Quest start")
+		Dialogic.set_variable("Npc/Mohamed/MohamedCurrentQuestDone", "false")
+		chiplings_killed_start = Stats.chiplings_killed
+	
+	# Quest done check
+	elif quest.id_name == "Mohamed1" and quest.active:
+		print("Quest check")
+		if Stats.chiplings_killed - chiplings_killed_start >= 2:
+			Dialogic.set_variable("Npc/Mohamed/MohamedCurrentQuestDone", "true")
+			Dialogic.set_variable("Npc/Mohamed/MohamedQuestStarted", "false")
+			Dialogic.set_variable("Npc/Mohamed/MohamedCurrentQuest", "1")
+			quest.completed = true
+			PlayerGlobals.coins += 250
+
+
+
+
+
+
 
 
 func check_dependencies(string: String):
@@ -122,6 +201,7 @@ func check_dependencies(string: String):
 				Dialogic.set_variable(quest.dialogic_next_quest_avalible_name, "true")
 			else:
 				Dialogic.set_variable(quest.dialogic_next_quest_avalible_name, "false")
+
 
 
 func get_quest_variables():
