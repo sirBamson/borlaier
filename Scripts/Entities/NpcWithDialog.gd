ff2c5a6fd358ca922e511cdda8b58903c59462a5
sirVader <alvar.m.sjogren@gmail.com>
2023-03-02 00:19:16 +0100

    Changed npc system

---
+++ Scripts/Entities/NpcWithDialog.gd
@@ -0,0 +1,26 @@
+extends StaticBody2D
+
+
+export (String) var npc_name: String = "npc name here"
+
+
+var in_quest_area: bool = false
+
+
+
+func _physics_process(_delta: float) -> void:
+	if in_quest_area:
+		if Input.is_action_pressed("talk") and not PlayerGlobals.talking:
+			PlayerGlobals.talking = true
+			Dialogic.set_variable("CurrentChar", npc_name)
+			add_child(Dialogic.start("Npc"))
+
+
+func _on_QuestArea_area_entered(area: Area2D) -> void:
+	if area.get_parent().is_in_group("Player"):
+		in_quest_area = true
+
+
+func _on_QuestArea_area_exited(area: Area2D) -> void:
+	if area.get_parent().is_in_group("Player"):
+		in_quest_area = false
