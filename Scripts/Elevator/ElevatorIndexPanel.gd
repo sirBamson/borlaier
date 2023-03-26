4f5cf9f4bcba8d14f2e1944db554833f74e6d33a
sirVader <alvar.m.sjogren@gmail.com>
2023-03-26 19:08:33 +0200

    Removed old code

--- Scripts/Elevator/ElevatorIndexPanel.gd
+++ Scripts/Elevator/ElevatorIndexPanel.gd
@@ -16,28 +16,6 @@ func _ready() -> void:
 Beroende på om hissen körs upp eller ner så sätts pilen i hissen till olika håll.
 Sätter även vilket number som ska visas av AnimatedSprite.
 """
-"""
-func _physics_process(_delta: float) -> void:
-	
-	if EnvVar.elevator_old_level_number != EnvVar.elevator_oldest_level_number:
-		
-		if EnvVar.elevator_current_level_number > EnvVar.elevator_old_level_number:
-			EnvVar.elevator_oldest_level_number = EnvVar.elevator_old_level_number
-			$ArrowUp.visible = true
-			$AnimatedSprite.frame = (EnvVar.elevator_old_level_number * 11)
-			$AnimatedSprite.play("FloorIndex")
-			
-		if EnvVar.elevator_current_level_number < EnvVar.elevator_old_level_number:
-			EnvVar.elevator_oldest_level_number = EnvVar.elevator_old_level_number
-			$ArrowDown.visible = true
-			$AnimatedSprite.frame = (EnvVar.elevator_old_level_number * 11)
-			$AnimatedSprite.play("FloorIndex", true)
-		
-	if $AnimatedSprite.frame == (EnvVar.elevator_button_number_pressed * 11):
-		$ArrowDown.visible = false
-		$ArrowUp.visible = false
-		$AnimatedSprite.stop()
-"""
 
 
 func set_index(to: int, from: int):
