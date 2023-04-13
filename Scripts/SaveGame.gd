fa48c8305e91b714818f5b93d1c04dddcafa0d26
sirVader <alvar.m.sjogren@gmail.com>
2023-04-14 01:37:44 +0200

    Fixed game crashing bug

--- Scripts/SaveGame.gd
+++ Scripts/SaveGame.gd
@@ -8,7 +8,6 @@ const SAVE_FILE_DIR: String = "res://DEBUG"
 
 var game_data: Dictionary = {}
 
-
 func _ready() -> void:
 	load_data()
 
@@ -77,6 +76,8 @@ Hämtar alla variablar som ska laddas och lägger de i en dict.
 
 func get_game_data() -> void:
 	game_data = {
+		"Last_save": game_data,
+		
 		# Player data
 		"Player_health": PlayerGlobals.health,
 		"Player_has_gun": PlayerGlobals.has_gun,
@@ -133,7 +134,8 @@ func get_game_data() -> void:
 		"Quest_FlatCap2_dialogic": QuestController.FlatCap2_dialogic,
 		"Quest_FlatCap21_dict": QuestController.FlatCap21_dict,
 		"Quest_FlatCap22_dict": QuestController.FlatCap22_dict,
-		}
+		
+	}
 
 
 """
@@ -141,51 +143,61 @@ Sätter alla variablar som är sparade.
 """
 
 func set_game_data() -> void:
-	PlayerGlobals.health = game_data["Player_health"]
-	PlayerGlobals.has_gun = game_data["Player_has_gun"]
-	PlayerGlobals.talking = game_data["Player_talking"]
-	PlayerGlobals.coins = game_data["Player_coins"]
-	PlayerGlobals.magazine_size = game_data["Player_magazine_size"]
-	PlayerGlobals.bullets_in_mag = game_data["Player_bullets_in_mag"]
-	PlayerGlobals.bullets_left = game_data["Player_bullets_left"]
-	PlayerGlobals.grenades_left = game_data["Player_grenades_left"]
-	PlayerGlobals.holding_grenade = game_data["Player_holding_grenade"]
-	PlayerGlobals.current_weapon = game_data["Player_current_weapon"]
-	PlayerGlobals.elevator_floor_access = game_data["Player_elevator_floor_access"]
-	PlayerGlobals.weapon_tier = game_data["Player_weapon_tier"]
+	var all_good: bool = true
+	for key in game_data["Last_save"]:
+		if !game_data.has(key):
+			all_good = false
 	
-	EnvVar.first_time_played = game_data["EnvVar_first_time_played"]
-	EnvVar.latest_level_path = game_data["EnvVar_latest_level_path"]
-	EnvVar.minigame_load_scene_path = game_data["EnvVar_minigame_load_scene_path"]
-	EnvVar.elevator_next_level = game_data["EnvVar_elevator_next_level"]
-	EnvVar.elevator_current_level_number = game_data["EnvVar_elevator_current_level_number"]
-	EnvVar.elevator_button_number_pressed = game_data["EnvVar_elevator_button_number_pressed"]
-	EnvVar.elevator_moving = game_data["EnvVar_elevator_moving"]
+	if all_good:
+		PlayerGlobals.health = game_data["Player_health"]
+		PlayerGlobals.has_gun = game_data["Player_has_gun"]
+		PlayerGlobals.talking = game_data["Player_talking"]
+		PlayerGlobals.coins = game_data["Player_coins"]
+		PlayerGlobals.magazine_size = game_data["Player_magazine_size"]
+		PlayerGlobals.bullets_in_mag = game_data["Player_bullets_in_mag"]
+		PlayerGlobals.bullets_left = game_data["Player_bullets_left"]
+		PlayerGlobals.grenades_left = game_data["Player_grenades_left"]
+		PlayerGlobals.holding_grenade = game_data["Player_holding_grenade"]
+		PlayerGlobals.current_weapon = game_data["Player_current_weapon"]
+		PlayerGlobals.elevator_floor_access = game_data["Player_elevator_floor_access"]
+		PlayerGlobals.weapon_tier = game_data["Player_weapon_tier"]
+		
+		EnvVar.first_time_played = game_data["EnvVar_first_time_played"]
+		EnvVar.latest_level_path = game_data["EnvVar_latest_level_path"]
+		EnvVar.minigame_load_scene_path = game_data["EnvVar_minigame_load_scene_path"]
+		EnvVar.elevator_next_level = game_data["EnvVar_elevator_next_level"]
+		EnvVar.elevator_current_level_number = game_data["EnvVar_elevator_current_level_number"]
+		EnvVar.elevator_button_number_pressed = game_data["EnvVar_elevator_button_number_pressed"]
+		EnvVar.elevator_moving = game_data["EnvVar_elevator_moving"]
+		
+		Stats.chiplings_killed = game_data["Stats_chiplings_killed"]
+		Stats.bullets_fired = game_data["Stats_bullets_fired"]
+		Stats.grenades_thrown = game_data["Stats_grenades_thrown"]
+		Stats.kgbs_times_won = game_data["Stats_kgbs_times_won"]
+		
+		scene_controller.elevator_variables = game_data["LevelVar_elevator_variables"]
+		scene_controller.level0_variables = game_data["LevelVar_level0_variables"]
+		scene_controller.level1_variables = game_data["LevelVar_level1_variables"]
+		scene_controller.level2_variables = game_data["LevelVar_level2_variables"]
+		scene_controller.level3_variables = game_data["LevelVar_level3_variables"]
+		
+		QuestController.FatMan_dialogic = game_data["Quest_FatMan_dialogic"]
+		QuestController.FatMan1_dict = game_data["Quest_FatMan1_dict"]
+		QuestController.FatMan2_dict = game_data["Quest_FatMan2_dict"]
+		
+		QuestController.Bartender2_dialogic = game_data["Quest_Bartender2_dialogic"]
+		QuestController.Bartender21_dict = game_data["Quest_Bartender21_dict"]
+		
+		QuestController.Mohamed_dialogic = game_data["Quest_Mohamed_dialogic"]
+		QuestController.Mohamed1_dict = game_data["Quest_Mohamed1_dict"]
+		
+		QuestController.FlatCap_dialogic = game_data["Quest_FlatCap_dialogic"]
+		QuestController.FlatCap1_dict = game_data["Quest_FlatCap1_dict"]
+		
+		QuestController.FlatCap2_dialogic = game_data["Quest_FlatCap2_dialogic"]
+		QuestController.FlatCap21_dict = game_data["Quest_FlatCap21_dict"]
+		QuestController.FlatCap22_dict = game_data["Quest_FlatCap22_dict"]
 	
-	Stats.chiplings_killed = game_data["Stats_chiplings_killed"]
-	Stats.bullets_fired = game_data["Stats_bullets_fired"]
-	Stats.grenades_thrown = game_data["Stats_grenades_thrown"]
-	Stats.kgbs_times_won = game_data["Stats_kgbs_times_won"]
-	
-	scene_controller.elevator_variables = game_data["LevelVar_elevator_variables"]
-	scene_controller.level0_variables = game_data["LevelVar_level0_variables"]
-	scene_controller.level1_variables = game_data["LevelVar_level1_variables"]
-	scene_controller.level2_variables = game_data["LevelVar_level2_variables"]
-	scene_controller.level3_variables = game_data["LevelVar_level3_variables"]
-	
-	QuestController.FatMan_dialogic = game_data["Quest_FatMan_dialogic"]
-	QuestController.FatMan1_dict = game_data["Quest_FatMan1_dict"]
-	QuestController.FatMan2_dict = game_data["Quest_FatMan2_dict"]
-	
-	QuestController.Bartender2_dialogic = game_data["Quest_Bartender2_dialogic"]
-	QuestController.Bartender21_dict = game_data["Quest_Bartender21_dict"]
-	
-	QuestController.Mohamed_dialogic = game_data["Quest_Mohamed_dialogic"]
-	QuestController.Mohamed1_dict = game_data["Quest_Mohamed1_dict"]
-	
-	QuestController.FlatCap_dialogic = game_data["Quest_FlatCap_dialogic"]
-	QuestController.FlatCap1_dict = game_data["Quest_FlatCap1_dict"]
-	
-	QuestController.FlatCap2_dialogic = game_data["Quest_FlatCap2_dialogic"]
-	QuestController.FlatCap21_dict = game_data["Quest_FlatCap21_dict"]
-	QuestController.FlatCap22_dict = game_data["Quest_FlatCap22_dict"]
+	else:
+		reset_data()
+		save_data()
